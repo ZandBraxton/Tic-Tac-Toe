6b0ffe4bbc089a9fa35101ee5b8511ddc8c2bd98
ZandBraxton <Zand.Braxton@gmail.com>
2021-08-01 17:03:48 -0500

    Added some win condition logic

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const playerFactory = (name, marker) => {
-    return {name, marker}
+    let moves = []
+    return {name, marker, moves}
 }
 
 
@@ -7,21 +8,29 @@ const playerFactory = (name, marker) => {
 const gameBoard = (() => {
     const markers = []
     const domBoard = document.querySelectorAll('.gametile')
-
+    //create players
     const Player1 = playerFactory("P1", "X")
     const Player2 = playerFactory("P2", "O")
-    let x = (Math.floor(Math.random() * 2) == 0)
+    console.log(Player1)
     let player = {}
-    if(x) {
-        player = Player1
-    } else {
-        player = Player2
+    randomPlayerStart()
+
+
+
+
+    function randomPlayerStart() {
+        let x = (Math.floor(Math.random() * 2) == 0)
+        if(x) {
+            player = Player1
+        } else {
+            player = Player2
+        }
     }
-
-
+        
     domBoard.forEach(tile => {
         tile.addEventListener('click', function() {
             let tile = this
+            // console.log(tile.id)
             placeMarker(tile)
         })
     })
@@ -29,22 +38,57 @@ const gameBoard = (() => {
     function switchPlayer() {
         if(player.marker === "X") {
             player = Player2
-            console.log(player)
         } else {
             player = Player1
-            console.log(player)
         }
     }
 
     function placeMarker(tile) {
-        tile.textContent = player.marker
-        switchPlayer()
+        if (tile.textContent != '') {
+            return
+        } else {
+            tile.textContent = player.marker
+            // console.log(tile.id)
+            player.moves.push(tile.id)
+            checkWinCondition()
+            switchPlayer()
+        }
     }
 
-
-
-
+    function checkWinCondition() {
+        const winCondition = [
+            ['1', '5', '9'], 
+            ['3', '5', '7'], 
+            ['1', '2', '3'],
+            ['4', '5', '6'],
+            ['7', '8', '9'],
+            ['1', '4', '7'],
+            ['2', '5', '8'],
+            ['3', '6', '9']
+    ]
+        
+        for (let i = 0; i < winCondition.length; i++) {
+            let j = 0   
+            n = winCondition[i][j]
+            console.log(player.moves)
+            if (player.moves.includes(n)) {
+                    j++
+                    n = winCondition[i][j]
+                    if (player.moves.includes(n)) {
+                        j++
+                        n = winCondition[i][j]
+                        if (player.moves.includes(n)) {
+                            console.log("You Win!") 
+                    } 
+                } 
+            } 
+        } 
+    }
     
+
+        
+
+
 
 })();
 
